965f86a563d3883d8828c107e6484fd080461feb
mlxchange <mlxchange@foxmail.com>
2018-09-11 18:03:42 +0800

    add noArg annotation to AuthorizationReq model

--- app/src/main/java/com/mlx/codereader/model/request/AuthorizationReq.kt
+++ app/src/main/java/com/mlx/codereader/model/request/AuthorizationReq.kt
@@ -1,11 +1,13 @@
 package com.mlx.codereader.model.request
 
+import com.mlx.codereader.annotation.NoArg
 import com.mlx.codereader.utils.Configs
 
 /**
  * Project:codeReader
  * Created by malingxiang on 2018/9/10.
  */
+@NoArg
 data class AuthorizationReq(
         var client_secret :String= Configs.Account.CLIENT_SECRET,
         var scopes :List<String> = Configs.Account.SCOPES,
